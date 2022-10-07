dd29d04038ace0db69ea4cbe94fd5ae52b6e6f5c
M-fkstv <108363820+M-fkstv@users.noreply.github.com>
2022-10-07 08:54:08 +0300

    Lesson 6 (#7)
    * Created 2 functions, problems whith last function
    
    * Created function deepEqual, didn't work
    
    * Created 'uniqCountry' function, needs improvement
    
    * Corrected 'uniqCountry' function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -142,7 +142,7 @@ for (let i = 0; i < colors.length; i++) {
 // text3.addEventListener("click", (event) => {
 //   event.target.style.color = "magenta";
 // });
-const re = /(^\d{4})-(\d{2})-(\d{2}$)/g;
+const re = /(^\d{4})-(\d{2})-(\d{2}$)/g; //Преобразование формата даты
 
 const date = "2020-11-26";
 
@@ -194,7 +194,7 @@ const data = [
   },
 ];
 
-const string = "Germany";
+const string = "Germany"; //Поиск объектов размещения
 const search = function () {
   for (let i = 0; i < data.length; i++) {
     const j = data[i];
@@ -210,3 +210,106 @@ const search = function () {
   }
 };
 search(string);
+
+const palindrome1 = () => word === word.split("").reverse().join("");
+console.log(palindrome1());
+
+console.log("---------------");
+
+import { hotels } from "./module.js";
+
+const hotelsInd = "USA"; //Поиск объектов размещения
+
+const filteredHotels = hotels.filter((item) => {
+  if (
+    item.country === hotelsInd || // можно так
+    item.city.includes(hotelsInd) === true || // можно и так
+    item.name.includes(hotelsInd) === true
+  ) {
+    console.log(item.country, item.city, item.name);
+    return `${item.country} ${item.city} ${item.name}`; // Как правильно??
+    // console.log([`${item.country} ${item.city} ${item.name}`]);
+    // return true; // {name: 'Hotel Leopold', city: 'Saint Petersburg', country: 'Russia'}
+  }
+});
+
+console.log(filteredHotels);
+
+console.log("---------------");
+//
+const uniqCountry = (hotels) => {
+  return hotels.reduce((acc, hotels) => {
+    if (hotels.country === "USA") {
+      acc.push(hotels.city);
+    }
+    return acc;
+  }, []);
+};
+console.log(uniqCountry(hotels));
+
+console.log("---------------");
+console.log(hotels[8].country === hotels[16].country);
+
+const uniqCountry1 = (hotels) => {
+  return hotels.reduce(
+    (uniqCountry1, hotels) => {
+      if (hotels.country === "USA") {
+        uniqCountry1.USA.push(hotels.city);
+      }
+      if (hotels.country === "Russia") {
+        uniqCountry1.Russia.push(hotels.city);
+      }
+      if (hotels.country === "Germany") {
+        uniqCountry1.Germany.push(hotels.city);
+      }
+      return uniqCountry1;
+    },
+    {
+      USA: [],
+      Russia: [],
+      Germany: [],
+    }
+  );
+};
+console.log(uniqCountry1(hotels));
+// console.log(`${hotels[i].country}':  ${["Chicago", "Hawaii", "Miami"]} }`);
+
+import { obj1 } from "./module.js";
+import { obj2 } from "./module.js";
+// import { obj3 } from "./module.js";
+
+const properties1 = Object.getOwnPropertyNames(obj1);
+const properties2 = Object.getOwnPropertyNames(obj2);
+// const properties3 = Object.getOwnPropertyNames(obj3);
+
+console.log(properties1);
+console.log(properties2);
+console.log(obj1[properties1[1]]);
+console.log(obj2[properties2[1]]);
+console.log(typeof properties1[0]);
+console.log(typeof properties2[0]);
+
+const deepEqual = (obj1, obj2) => {
+  if (properties1.length !== properties2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < properties1.length; i++) {
+    if (
+      typeof properties1[i] !== "object" &&
+      typeof properties2[i] !== "object" &&
+      obj1[properties1[i]] !== obj1[properties2[i]]
+    ) {
+      return false;
+    }
+    if (
+      typeof properties1[i] === "object" &&
+      typeof properties2[i] === "object" &&
+      !deepEqual(obj1[properties1[i]], obj2[properties2[i]]) // Почему не работает?
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(deepEqual(obj1, obj2));
