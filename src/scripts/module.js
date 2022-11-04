e45cc000f5867d12ba6391df30d98992e08871c9
M-fkstv <108363820+M-fkstv@users.noreply.github.com>
2022-11-04 08:58:47 +0300

    Lesson 10 (#10)
    * test commit
    
    * made layout 'Homes guest loves'  in js

--- src/scripts/module.js
+++ src/scripts/module.js
@@ -213,41 +213,71 @@ export const obj3 = {
   },
   b: "b",
 };
-// const properties1 = Object.getOwnPropertyNames(obj1);
-// const properties2 = Object.getOwnPropertyNames(obj2);
-// const properties3 = Object.getOwnPropertyNames(obj3);
-//
-// console.log(properties1);
-// console.log(properties2);
-// console.log(obj1[properties1[1]]);
-// console.log(obj2[properties2[0]]);
-// console.log(typeof properties1[0]);
-// console.log(typeof properties2[0]);
 
-// const deepEqual = (obj1, obj2) => {
-//   const properties1 = Object.getOwnPropertyNames(obj1);
-//   const properties2 = Object.getOwnPropertyNames(obj2);
-//   if (properties1.length !== properties2.length) {
-//     return false;
-//   }
-//
-//   for (let i = 0; i < properties1.length; i++) {
-//     if (
-//       typeof properties1[i] !== "object" &&
-//       typeof properties2[i] !== "object" &&
-//       obj1[properties1[i]] !== obj1[properties2[i]]
-//     ) {
-//       return false;
-//     }
-//     if (
-//       typeof properties1[i] === "object" &&
-//       typeof properties2[i] === "object"
-//       // !deepEqual(obj1[properties1[i]], obj2[properties2[i]]) // Почему не работает?
-//     ) {
-//       !deepEqual(obj1[properties1[i]], obj2[properties2[i]]);
-//       // return false;
-//     }
-//   }
-//   return true;
-// };
-// console.log(deepEqual(obj1, obj2));
+export const data1 = [
+  {
+    id: "71ce9eac-e9b9-44f0-a342-9ff0b565f3b7",
+    name: "Hotel Leopold",
+    city: "Saint Petersburg",
+    country: "Russia",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg",
+  },
+
+  {
+    id: "aa560608-a879-48a7-80b6-deff2806b250",
+    name: "Apartment Sunshine",
+    city: "Santa  Cruz de Tenerife",
+    country: "Spain",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg",
+  },
+  {
+    id: "1d88fefe-edf1-4cda-844a-babbf29bb2b3",
+    name: "Villa Kunerad",
+    city: "Vysokie Tatry",
+    country: "Slowakia",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg",
+  },
+  {
+    id: "a2bf824d-edd8-41f0-8b70-244334086ab4",
+    name: "Hostel Friendship",
+    city: "Berlin",
+    country: "Germany",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg",
+  },
+  {
+    id: "4024535d-a498-4274-b7cb-f01ada962971",
+    name: "Radisson Blu Hotel",
+    city: "Kyiv",
+    country: "Ukraine",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg",
+  },
+  {
+    id: "e51e71f6-6baf-4493-b3ae-25dc27cdc238",
+    name: "Paradise Hotel",
+    city: "Guadalupe",
+    country: "Mexico",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg",
+  },
+  {
+    id: "87d2b966-2431-43f3-8c0d-2c8723474dfc",
+    name: "Hotel Grindewald",
+    city: "Interlaken",
+    country: "Switzerland",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg",
+  },
+  {
+    id: "190221c6-b18f-4dba-97de-e35f0e14c023",
+    name: "The Andaman Resort",
+    city: "Port Dickson",
+    country: "Malaysia",
+    imageUrl:
+      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg",
+  },
+];
